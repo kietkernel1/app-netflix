2d96e3d481672afed65c53eddfc3adaebb4f0482
kietkernel1 <kietkernel1@gmail.com>
2023-01-22 20:18:58 +0700

    first commit

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const cors = require("cors");
 dotenv.config();
 // ["https://client-netflix-8aaujho2e-kietkernel1.vercel.app","https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app"]
 app.use(cors({credentials: true, origin: "https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app"}));
+
+app.use(function (req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", ["https://client-netflix-8aaujho2e-kietkernel1.vercel.app","https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app"])
+    res.setHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with")
+    res.setHeader("Access-Control-Allow-Credentials", true)
+    next()
+})
+
 app.use(cookieParser())
 
 app.use(express.json())
