e1a685380d2f03be6bb6613798701c4479dc0533
kietkernel1 <kietkernel1@gmail.com>
2023-01-23 12:39:30 +0700

    first commit

--- index.js
+++ index.js
@@ -12,16 +12,10 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors");
 
 dotenv.config();
-// ["https://client-netflix-8aaujho2e-kietkernel1.vercel.app","https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app"]
-// app.use(cors({credentials: true, origin: "https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app"}));
 
-app.use(function (req, res, next){
-    res.setHeader("Access-Control-Allow-Origin", "https://admin-netflix-iku2v7qw3-kietkernel1.vercel.app")
-    res.setHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with")
-    res.setHeader("Access-Control-Allow-Credentials", true)
-    next()
-})
+app.use(cors({credentials: true, origin: ["https://admin-netflix.vercel.app/","https://client-netflix.vercel.app/"]}));
+
+
 
 app.use(cookieParser())
 
